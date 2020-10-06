228f29bf97bdac560e5e43a3f3724710d497ad1c
MainaGeorge <nuriszka@gmail.com>
2020-10-06 22:43:15 +0200

    Added some more linked link methods

--- Program.cs
+++ Program.cs
@@ -10,15 +10,14 @@ namespace dojo
             var linked = new LinkedLIst<int>();
 
             linked.AddLast(7);
-            // linked.AddFirst(12);
-            // linked.RemoveAt(1);
-            // linked.InsertAt(1, 8);
-            // linked.InsertAt(1, 15);
-            // linked.RemoveAt(3);
+            linked.AddLast(8);
+            linked.AddLast(9);
+            linked.AddLast(10);
+            linked.AddLast(11);
             linked.PrintValues();
-            linked.BetterReverse();
-            Console.WriteLine("after reverse");
-            linked.PrintValues();
+            
+            Console.WriteLine(linked.GetNthNodeFromEnd(6).Value + "4");
+
 
 
             Console.WriteLine($"the list has {linked.Size} elements");
