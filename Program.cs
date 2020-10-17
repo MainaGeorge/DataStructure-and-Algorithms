15a2f9233bef78f3b729bbb427bfc53ecad33f87
MainaGeorge <nuriszka@gmail.com>
2020-10-17 13:41:31 +0200

    Added some more binary tree algorithms

--- Program.cs
+++ Program.cs
@@ -7,17 +7,39 @@ namespace dojo
     {
         private static void Main()
         {
-            var prior = new ArrayPriorityQueue();
-            prior.Add(10);
-            prior.Add(5);
-            prior.Add(3);
-            prior.Add(12);
+            var tree = new Tree();
 
-            prior.Remove();
-            prior.Remove();
-            Console.WriteLine(prior.Peek());
+            tree.Insert(7);
+            tree.Insert(4);
+            tree.Insert(9);
+            tree.Insert(1);
+            tree.Insert(6);
+            tree.Insert(8);
+            tree.Insert(10);
+            tree.Insert(14);
+            tree.Insert(12);
+            tree.Insert(15);
 
-            Console.WriteLine(prior);
+            Console.WriteLine("they are siblings " + tree.AreSiblings(8, 6));
+
+
+
+            var tree2 = new Tree();
+            tree2.Insert(7);
+            tree2.Insert(4);
+            tree2.Insert(9);
+            tree2.Insert(3);
+            tree2.Insert(6);
+            tree2.Insert(8);
+            tree2.Insert(10);
+            tree2.Insert(11);
+            tree2.Insert(12);
+
+
+
+            Console.WriteLine("the height is " + tree.Height());
+            Console.WriteLine(tree.CountLeaves());
+
         }
 
 
