700212fe109cc74cbe8488887629710f77c6f87a
MainaGeorge <nuriszka@gmail.com>
2020-10-22 18:20:39 +0200

    Added some random functions

--- Program.cs
+++ Program.cs
@@ -16,11 +16,11 @@ namespace dojo
             tree.Insert(6);
             tree.Insert(8);
             tree.Insert(10);
-            tree.Insert(14);
-            tree.Insert(12);
-            tree.Insert(15);
+            // tree.Insert(14);
+            // tree.Insert(12);
+            // tree.Insert(15);
 
-            Console.WriteLine("they are siblings " + tree.AreSiblings(8, 6));
+            Console.WriteLine("does the sum exist? " + tree.ExistPathLeadingToGivenSum(112));
 
 
 
